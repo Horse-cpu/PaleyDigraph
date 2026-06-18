b20b260eb54829c4476a180a1ce9938372587f30
Horse <55663821+Horse-cpu@users.noreply.github.com>
2026-06-18 18:22:42 +0100

    Update Paley.py

--- Paley.py
+++ Paley.py
@@ -6,8 +6,6 @@ nonzero = [i for i in range(1,n)]
 squares = [(i*i)%n for i in range(1,int(n/2+1))]
 squares.sort()
 print("SQUARES: " + str(squares))
-nonsqua = [i for i in range(1,n) if i not in squares]
-paley = [(a,b) for a in range(0,n) for b in range(0,n) if (b-a)%n in squares]
 
 
 ## check for every pair (0,x) how many vertices of the graph point to both 0 and x
@@ -19,6 +17,8 @@ for i in range(1,n):
             continue
         elif (j-i)%n in squares:
             dominators2[i-1] += 1
+    if dominators2[i-1] == 0:
+        print("NOT 2")
 print("2-DOMS: " + str(dominators2))
 
 ## the same check for triples (0,x,y)
@@ -31,6 +31,8 @@ for i in combinations(nonzero,2):
             continue
         elif ((j-i[0])%n in squares) and ((j-i[1])%n in squares):
             dominators3[cnt] += 1
+    if dominators3[cnt] == 0:
+        print("NOT 3")
     cnt += 1
 print("3-DOMS: " + str(dominators3))
 
@@ -45,13 +47,27 @@ for i in combinations(nonzero,3):
         elif ((j-i[0])%n in squares) and ((j-i[1])%n in squares) and ((j-i[2])%n in squares):
             dominators4[cnt] += 1
     if dominators4[cnt] == 0:
-        print("BAD")
-        exit()
+        print("NOT 4")
     cnt += 1
 print("4-DOMS: " + str(dominators4))
 
-## this doesn't print "BAD", so every 4-set (0,x,y,z) has atleast one vertex pointing
-## at all four, hence the n=67 Paley digraph is a 4-paradox tournament.
+## the 5 check is commented out as it can take a very long time to run
+'''
+## the check for 5-sets (0,x,y,z)
+dominators5 = []
+cnt = 0
+for i in combinations(nonzero,4):
+    dominators5.append(0)
+    for j in squares:
+        if j == i[0] or j == i[1] or j == i[2] or j == i[3]:
+            continue
+        elif ((j-i[0])%n in squares) and ((j-i[1])%n in squares) and ((j-i[2])%n in squares) and ((j-i[3])%n in squares):
+            dominators5[cnt] += 1
+    if dominators5[cnt] == 0:
+        print("NOT 5")
+    cnt += 1
+print("5-DOMS: " + str(dominators5))
+'''
 
 
 
